d35f138c515dd42afea7d554cd29d3e78e68d178
John Hocker <me@johnhocker.com>
2026-01-02 17:48:25 -0500

    fixe email rendering

--- includes/email-rendering.php
+++ includes/email-rendering.php
@@ -1,13 +1,17 @@
 <?php
 
 add_action(
-	'getpaid_after_email_details_invoice_total',
-	'gpes_add_emails_to_invoice_email',
-	10,
-	2
-);
+		'getpaid_after_email_details_invoice_total',
+		'gpes_add_emails_to_invoice_email',
+		10,
+		2
+		);
 
-function gpes_add_emails_to_invoice_email($invoice, $data) {
+function gpes_add_emails_to_invoice_email( $invoice, $data ) {
+
+	if ( ! is_object( $invoice ) ) {
+		return;
+	}
 
 	$emails = gpes_get_emails_for_invoice( $invoice );
 
@@ -15,17 +19,16 @@ function gpes_add_emails_to_invoice_email($invoice, $data) {
 		return;
 	}
 
-	$template = '
-		<tr class="getpaid-email-details-'.GPESGPES_META_EMAILS.'">
-			<td class="getpaid-lable-td">
-				Email addresses covered
-			 </td>
-			<td class="getpaid-value-td">
-				<span class="getpaid-invoice-meta-'.GPESGPES_META_EMAILS.'-value">'.gpes_emails_to_html_lines( $emails ).'</span>
-			</td>
-		</tr>
-	';
-
-	echo $template;
-
+	?>
+    <tr class="getpaid-email-details-<?php echo esc_attr( GPES_META_EMAILS ); ?>">
+        <td class="getpaid-label-td">
+            <?php esc_html_e( 'Email addresses covered', 'getpaid-email-subscriptions' ); ?>
+        </td>
+        <td class="getpaid-value-td">
+            <span class="getpaid-invoice-meta-<?php echo esc_attr( GPES_META_EMAILS ); ?>-value">
+                <?php echo gpes_emails_to_html_lines( $emails ); ?>
+            </span>
+        </td>
+    </tr>
+    <?php
 }
